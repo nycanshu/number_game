cf9ee15a57591a6a0d8c60314d1b954cdfdbd656
Himanshu <112685847+nycanshu@users.noreply.github.com>
2023-07-14 11:17:53 +0530

    Add files via upload

---
+++ Number_game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.number_game;
+
+/**
+ *
+ * @author HIMANSHU
+ */
+public class Number_game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
